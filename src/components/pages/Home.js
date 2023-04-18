e3209386e25b0a96e4e2d18c4dbcc490403dbc77
Katrina <katrossi414@gmail.com>
2023-04-18 14:39:26 -0500

    home page modified

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
   return (
     <div>
       <section ref={homeRef} id="homePage" className={styles.container}>
-        <img className={styles.tab} src="/images/homepage_tab.png" />
-        <div className={styles.trinaContainer}>
-          <img className={styles.trina} src="/images/trina-no-border.png" />
+        <div className={styles.tab}>
+          <img className={styles.hiThere} src="/images/hi-there.png"/>
+          <div className={styles.trinaContainer}>
+            <img className={styles.trina} src="/images/trina-no-border.png" />
+          </div>
+          <img className={styles.banner} src="/images/dev-des-banner.png"/>
         </div>
+        <div className={styles.bottom}></div>
       </section>
     </div>
   );
