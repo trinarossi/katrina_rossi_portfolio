8e3be76d7df0f081a1df3373f67b6e1bf5b6eb52
Katrina <katrossi414@gmail.com>
2023-04-18 14:01:01 -0500

    contact page styled

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,8 +8,31 @@ const Contact = () => {
   return (
     <div>
       <section ref={contactRef} id="contactPage" className={styles.container}>
-        <h1>Contact</h1>
-        <h2></h2>
+        <div className={styles.tab}>
+          <div className={styles.contact}>
+            <img className={styles.icon} src="/images/icons/email.png" />
+            <p>katrinarossidev@gmail.com</p>
+          </div>
+          <div className={styles.contact}>
+            <img className={styles.icon} src="/images/icons/linkedin.png" />
+            <a
+              href="https://www.linkedin.com/in/katrina-rossi/"
+              target="_blank"
+            >
+              <p>linkedin.com/in/katrina-rossi</p>
+            </a>
+          </div>
+          <div className={styles.contact}>
+            <img
+              className={styles.icon}
+              src="/images/icons/github-purple.png"
+            />
+            <a href="https://github.com/trinarossi" target="_blank">
+              <p>github.com/trinarossi</p>
+            </a>
+          </div>
+          <img className={styles.thanks} src="/images/thanks.png" />
+        </div>
       </section>
     </div>
   );
